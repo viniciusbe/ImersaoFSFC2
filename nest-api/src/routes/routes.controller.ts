68f2afc2aae176daf8086443750d1add5f0cf0c4
viniciusbe <vinicius.bernardesds@gmail.com>
2021-04-21 17:57:57 -0300

    feat: create routes

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
@@ -30,5 +38,10 @@ export class RoutesController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.routesService.remove(+id);
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    console.log(id);
   }
 }
--- nest-api/src/routes/routes.module.ts
+++ nest-api/src/routes/routes.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { Route, RouteSchema } from './entities/route.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+  ],
   controllers: [RoutesController],
-  providers: [RoutesService]
+  providers: [RoutesService],
 })
 export class RoutesModule {}
